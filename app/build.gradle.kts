c30d7c9f30577f774da2e2ad8f13682816ae0872
Minimal-Alexi <alexpop@metropolia.fi>
2025-10-28 13:42:01 +0200

    Added google maps and gradel secrets.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.appcompat)
+    implementation(libs.play.services.maps)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
