4190a43b31543d617020dddecb6fcf272dbf9ab0
Runzhou Zhu <runzhouz@metropolia.fi>
2025-11-02 18:55:16 +0200

    add route on map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val apiKey: String = project.findProperty("MAPS_API_KEY") as? String ?: ""
+        buildConfigField("String", "MAPS_API_KEY", "\"${project.findProperty("MAPS_API_KEY") ?: ""}\"")
     }
 
     buildTypes {
@@ -44,6 +47,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
--- app/src/main/java/com/example/mapapp/data/network/RoutesApiService.kt
+++ app/src/main/java/com/example/mapapp/data/network/RoutesApiService.kt
@@ -1,6 +1,8 @@
 package com.example.mapapp.data.network
 
+import com.example.mapapp.BuildConfig
 import com.example.mapapp.data.model.*
+
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.Headers
@@ -11,7 +13,7 @@ interface RoutesApiService {
     @POST("directions/v2:computeRoutes")
     suspend fun computeRoutes(
         @Body request: RoutesRequest,
-        @Header("X-Goog-Api-Key") apiKey: String = "",
+        @Header("X-Goog-Api-Key") apiKey: String = BuildConfig.MAPS_API_KEY,
         @Header("X-Goog-FieldMask") fieldMask: String = "",
     ): RoutesResponse
 }
