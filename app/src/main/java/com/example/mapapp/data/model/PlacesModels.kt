e7bbc275d466bdb67ee07b346dd1a646e69f1d43
Minimal-Alexi <alexpop@metropolia.fi>
2025-11-02 17:34:14 +0200

    Adding places imports. Created final response headers. using a bit of Zhu's code for help.

--- app/src/main/java/com/example/mapapp/data/model/PlacesModels.kt
+++ app/src/main/java/com/example/mapapp/data/model/PlacesModels.kt
@@ -1,8 +1,14 @@
 package com.example.mapapp.data.model
 
+import com.google.android.libraries.places.api.model.Place
+
 data class PlacesRequest(
     val maxResultCount:Int = 10,
     val locationRestriction: LocationRestriction
+)
+
+data class PlacesResponse(
+    val places:List<Place>?
 )
 
 data class LocationRestriction(
--- app/src/main/java/com/example/mapapp/data/network/PlacesApiService.kt
+++ app/src/main/java/com/example/mapapp/data/network/PlacesApiService.kt
@@ -3,12 +3,15 @@ package com.example.mapapp.data.network
 import com.example.mapapp.data.model.*
 import retrofit2.http.Body
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface PlacesApiService {
+    @Headers("Content-Type: application/json")
     @POST("/v1/places:searchNearby")
     suspend fun computeRoutes(
-        @Body request: RoutesRequest,
-        @Header("X-Goog-Api-Key") apiKey: String
-    ): RoutesResponse
+        @Body request: PlacesRequest,
+        @Header("X-Goog-Api-Key") apiKey: String,
+        @Header("X-Goog-FieldMask") fieldMask: String
+    ): PlacesResponse
 }
