ef8b3af753ff9008381a11eb9e278ef429796b8b
Minimal-Alexi <alexpop@metropolia.fi>
2025-11-16 16:29:42 +0200

    Creating geocodingApi

---
+++ app/src/main/java/com/example/mapapp/data/network/GeocodingApi.kt
@@ -0,0 +1,27 @@
+package com.example.mapapp.data.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object GeocodingApi {
+    private const val BASE_URL = "https://maps.googleapis.com/maps/api/geocode"
+
+    val service: PlacesApiService by lazy {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PlacesApiService::class.java)
+    }
+}
--- app/src/main/java/com/example/mapapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mapapp/viewmodel/HomeViewModel.kt
@@ -2,6 +2,29 @@ package com.example.mapapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mapapp.data.location.DefaultLocationClient
+import com.example.mapapp.data.location.LocationClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.launch
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
+
+
+    private val locationClient: LocationClient =
+        DefaultLocationClient(
+            application,
+            LocationServices.getFusedLocationProviderClient(application)
+        )
+
+    /*TODO: Figure out how to not bomb the GeoCode API with thousands of requests.*/
+//    init {
+//        viewModelScope.launch {
+//            locationClient.getLocationUpdates(10000L)
+//                .collect { location ->
+//                    _userLocation.value = LatLng(location.latitude, location.longitude)
+//                }
+//        }
+//    }
 }
